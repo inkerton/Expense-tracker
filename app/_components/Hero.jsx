0aef44e4c0e14a9df929a7e4b371b7b52f246e2f
Janvi <janvichoudhary116@gmail.com>
2024-07-06 17:08:54 +0530

    integrating clerk
    User Authentication

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="bg-gray-50 flex items-center flex-col">
+    <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+        <h1 className="text-3xl  font-extrabold sm:text-5xl">
+            Understand Your Flow.
+            <strong className="font-extrabold text-primary sm:block">  Control your Progress </strong>
+        </h1>
+
+        <p className="mt-4 sm:text-xl/relaxed">
+            Start creating your own budgets & tasks. Measure your progess and get ahead.
+        </p>
+
+        <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+            className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+            href="#"
+            >
+            Get Started
+            </a>
+
+            <a
+            className="block w-full rounded px-12 py-3 text-sm font-medium text-black shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+            href="#"
+            >
+            Learn More
+            </a>
+        </div>
+        </div>
+    </div>
+    <Image 
+    src={'/dashboard.png'} 
+    alt='dashboard image'
+    width={1000}
+    height={700}
+    className='mt-5 rounded-xl border-2 mb-5'
+    />
+    </section>
+  )
+}
+
+export default Hero
